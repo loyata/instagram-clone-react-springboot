f8f44d3db5116086b11b8b3c45344b36b2a5d9ea
loyata <77038994+loyata@users.noreply.github.com>
2022-08-24 15:15:42 -0400

    first commit

---
+++ backend/src/test/java/ca/uottawa/ins/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.uottawa.ins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
